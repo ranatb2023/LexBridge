e74e1824e8e6b3e24f353c3eb2b32b2b917b2609
Muhammad Rana <ranatb2023@gmail.com>
2025-07-09 12:55:21 +0500

    Route Successfull

--- routes/stripeWebhookRoutes.js
+++ routes/stripeWebhookRoutes.js
@@ -4,6 +4,6 @@ const { handleStripeWebhook } = require("../controllers/stripeWebhookControllers
 const router = express.Router();
 
 // Stripe requires raw body parsing
-router.post("/webhook", express.raw({ type: "application/json" }), handleStripeWebhook);
+router.post("/", express.raw({ type: "application/json" }), handleStripeWebhook);
 
 module.exports = router;
